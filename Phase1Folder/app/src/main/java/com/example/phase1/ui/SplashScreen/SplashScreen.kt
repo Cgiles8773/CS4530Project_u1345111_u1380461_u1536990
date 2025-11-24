8c6b67b698cd1f03f9e97646d62e3673f82356f8
Eric Nguyen <eric.nguyen.kt424@gmail.com>
2025-11-24 15:36:22 -0700

    Implemented splash screen animation for portrait screen

--- Phase1Folder/app/src/main/java/com/example/phase1/ui/SplashScreen/SplashScreen.kt
+++ Phase1Folder/app/src/main/java/com/example/phase1/ui/SplashScreen/SplashScreen.kt
@@ -9,42 +9,104 @@
 
 package com.example.phase1.ui.SplashScreen
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOutBack
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.phase1.ui.theme.*
+import kotlinx.coroutines.delay
+import kotlin.math.max
 
 @Composable
 fun SplashScreen(navController: NavController) {
 
-    // Trigger navigation after 1 second
+    // Trigger navigation after animation finishes
     LaunchedEffect(Unit) {
-        kotlinx.coroutines.delay(1000) // 1 second
+        kotlinx.coroutines.delay(1800) // 2.05s
         navController.navigate("home") {
             popUpTo("splash") { inclusive = true } // remove splash from back stack
         }
     }
 
     // UI of splash screen
+    RisingBallAnimation()
+
+}
+
+@Composable
+fun RisingBallAnimation() {
+    val config = LocalConfiguration.current
+    val density = LocalDensity.current
+
+    val screenHeightPx = with(density) { config.screenHeightDp.dp.toPx() }
+    val risePx = screenHeightPx * 0.50f
+
+    // animate vertical offset only (ball stays centered horizontally)
+    val offsetY = remember { Animatable(0f) }
+
+    // animate the scale (1f = normal size)
+    val scale = remember { Animatable(1f) }
+
+    LaunchedEffect(Unit) {
+        // 1) Ball Rises upward
+        offsetY.animateTo(
+            targetValue = -risePx,
+            animationSpec = tween(
+                durationMillis = 1200,
+                easing = EaseOutBack
+            )
+        )
+
+        // Brief pause, then expand the ball
+        delay(50)
+        scale.animateTo(
+            targetValue = 12f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = EaseIn
+            )
+        )
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF6850A5)), // primary
-        contentAlignment = Alignment.Center
+            .background(Primary),
+        contentAlignment = Alignment.BottomCenter
     ) {
-        Text(
-            text = "Demo Splash Screen",
-            color = Color.White,
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
+        Box(
+            modifier = Modifier
+                .offset { IntOffset(0, offsetY.value.toInt()) }
+                .size(100.dp)
+                .graphicsLayer {
+                    scaleX = scale.value
+                    scaleY = scale.value
+                    transformOrigin = TransformOrigin.Center
+                }
+                .background(OnPrimary, CircleShape)
         )
     }
 }
--- Phase1Folder/app/src/main/java/com/example/phase1/ui/theme/Color.kt
+++ Phase1Folder/app/src/main/java/com/example/phase1/ui/theme/Color.kt
@@ -14,3 +14,13 @@ import androidx.compose.ui.graphics.Color
 val Primary = Color(0xFF6850A5)
 val OnPrimary = Color(0xFFFFFFFF)
 val PrimaryContainer = Color(0xFFeadeff)
+
+val Secondary = Color(0xFF615b70)
+val OnSecondary = OnPrimary
+val SecondaryContainer = Color(0xFFe8e0f2)
+
+val Tertiary = Color(0xFF805160)
+val OnTertiary = OnSecondary
+val TertiaryContainer = Color(0xFF805160)
+
+val Background = Color(0xFFfdf6ff)
