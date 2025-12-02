37087ca42cfeb5b12b39aa5949f22c93e95184fe
Eric Nguyen <eric.nguyen.kt424@gmail.com>
2025-12-01 19:13:55 -0700

    Community images now properly show

--- Phase1Folder/app/src/main/java/com/example/phase1/ui/communityscreen/CommunityScreen.kt
+++ Phase1Folder/app/src/main/java/com/example/phase1/ui/communityscreen/CommunityScreen.kt
@@ -7,18 +7,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -26,32 +22,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.example.phase1.ui.homescreen.shareImage
 import com.example.phase1.vm.DrawingViewModel
 import com.example.phase1.vm.HomeViewModel
-import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.tasks.await
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
 
-private data class Drawing(val uri: Uri, val title: String, val author: String, val filePath: String)
+private data class Drawing(
+    val uri: Uri,
+    val title: String,
+    val userID: String,
+)
 
 @Composable
 fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel, navController: NavController) {
+    Log.d("CommunityScreen","Checkpoint 1")
     val configuration = LocalConfiguration.current
     val screenWidth = configuration.screenWidthDp
     val screenHeight = configuration.screenHeightDp
@@ -59,33 +58,81 @@ fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel
     val itemHeight = screenHeight - 24
     val context = LocalContext.current
     val storage = Firebase.storage
+    val db = Firebase.firestore
+
+    Log.d("CommunityScreen","Checkpoint 2")
+    Log.d("PackageCheck", context.packageName)
+
+
+
 
     val user = viewModel.getUser()
+
+    // redirect if invalid
     if (user == null) {
         navController.navigate("login")
         return
     }
+
     val userId = user.uid
 
-    val communityDrawings by produceState<List<Drawing>>(initialValue = emptyList(), userId) {
-        val communityDrawingDocs = viewModel.getAllDocuments()
-        val drawings = mutableListOf<Drawing>()
-        communityDrawingDocs?.forEach { doc ->
-            val filePath = doc.getString("filePath")
-            val title = doc.getString("name") ?: "Untitled"
-            val author = doc.getString("authorName") ?: "Unknown"
-            if (filePath != null) {
-                try {
-                    val uri = storage.getReference(filePath).downloadUrl.await()
-                    drawings.add(Drawing(uri, title, author, filePath))
-                } catch (e: Exception) {
-                    Log.e("CommunityScreen", "Community image download failed: ${e.message}")
-                }
-            }
+    Log.d("CommunityScreen","Checkpoint 3")
+
+    // Testing starts
+
+
+    var dataString by remember { mutableStateOf("") }
+    LaunchedEffect(user) {
+        try {
+            val snapshot = db.collection("user_drawings").get().await()
+            val doc = snapshot.firstOrNull()
+            dataString = doc?.data?.toString() ?: "No data found"
         }
-        value = drawings
+        catch (e: Exception) {
+            Log.e("Firestore", "Error fetching", e)
+        }
     }
 
+    Column {
+        Text("Data string: $dataString")
+    }
+    // Testing ends
+
+
+    val communityDrawings by produceState(initialValue = emptyList(), userId) {
+        Log.d("CommunityScreen","Checkpoint 4")
+
+        val communityDrawingDocs = viewModel.getAllDocuments()
+        val drawings = mutableListOf<Drawing>()
+        Log.d("CommunityScreen","Checkpoint 5")
+
+        communityDrawingDocs?.forEach { doc ->
+            val title = doc.getString("title") ?: "Untitled"
+            val userID = doc.getString("userID") ?: "Unknown"
+            val imageUrl = doc.getString("URL")
+            // TODO: figure out timestamp implementation here
+            // TODO: figure out how to get author from userID
+
+            if (imageUrl != null) {
+                try {
+                    val uri = Uri.parse(imageUrl)
+                    Log.d("CommunityScreen", "$imageUrl, $title, $userID")
+
+                    drawings.add(Drawing(uri, title, userID))
+                } catch (e: Exception) {
+                    Log.e("CommunityScreen", "Community image conversion failed: ${e.message}")
+                }
+            } else {
+                Log.e("CommunityScreen", "Image URL missing for doc: ${doc.id}")
+            }
+        }
+
+        value = drawings
+        Log.d("CommunityScreen","Checkpoint 6")
+
+    }
+
+    Log.d("CommunityScreen","Checkpoint 7")
 
     // UI
     Column(
@@ -96,25 +143,10 @@ fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel
     ) {
         // Landscape layout
         if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            //Spacer(modifier = Modifier.height(17.dp))
-            Column(horizontalAlignment = Alignment.CenterHorizontally)
-            {
-                Text(
-                    text = "Community Browser",
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Button(
-                    onClick = { navController.navigate("home") },
-                ) { Text("Back Home") }
-            }
-            HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
             LazyRow(
-                modifier = Modifier
-                    .fillMaxSize()
+                modifier = Modifier.fillMaxSize()
             ) {
-                // Community Drawings
-                items(communityDrawings, key = { it.filePath }) { drawing ->
+                items(communityDrawings, key = { it.uri.toString() }) { drawing ->
                     Card(
                         modifier = Modifier.padding(horizontal = 4.dp, vertical = 4.dp)
                     ) {
@@ -123,7 +155,7 @@ fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             AsyncImage(
-                                model = drawing.uri.toString(),
+                                model = drawing.uri,
                                 contentDescription = drawing.title,
                                 modifier = Modifier
                                     .height(150.dp)
@@ -133,17 +165,18 @@ fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel
 
                             Column(
                                 verticalArrangement = Arrangement.SpaceEvenly,
-                                horizontalAlignment = Alignment.CenterHorizontally,
+                                horizontalAlignment = Alignment.Start,
                                 modifier = Modifier.padding(start = 10.dp)
                             ) {
                                 Text(text = drawing.title, style = MaterialTheme.typography.titleMedium)
-                                Text(text = "by ${drawing.author}", style = MaterialTheme.typography.bodySmall)
+                                Text(text = "by ${drawing.userID}", style = MaterialTheme.typography.bodySmall)
+//                                drawing.timestamp1?.let {
+//                                    Text(text = "Uploaded: $it", style = MaterialTheme.typography.bodySmall)
+//                                }
                                 Button(onClick = {
-                                    //TODO: Save image locally as a copy
                                     drawingViewModel.saveImageAsCopy(drawing.title, 1000, 1000)
                                     navController.navigate("main/$")
-                                })
-                                {
+                                }) {
                                     Text("Copy")
                                 }
                             }
@@ -151,28 +184,14 @@ fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel
                     }
                 }
             }
+
         }
         // Portrait layout
-        if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            Spacer(modifier = Modifier.height(17.dp))
-            Column(horizontalAlignment = Alignment.CenterHorizontally)
-            {
-                Text(
-                    text = "Community Browser",
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
-                Button(
-                    onClick = { navController.navigate("home") },
-                ) { Text("Back Home") }
-            }
-            HorizontalDivider(modifier = Modifier.padding(vertical = 16.dp))
+        else if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
             LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
+                modifier = Modifier.fillMaxSize()
             ) {
-                // Community Drawings
-                items(communityDrawings, key = { it.filePath }) { drawing ->
+                items(communityDrawings, key = { it.uri.toString() }) { drawing ->
                     Card(
                         modifier = Modifier.padding(horizontal = 4.dp, vertical = 4.dp)
                     ) {
@@ -181,7 +200,7 @@ fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             AsyncImage(
-                                model = drawing.uri.toString(),
+                                model = drawing.uri,
                                 contentDescription = drawing.title,
                                 modifier = Modifier
                                     .height(150.dp)
@@ -191,17 +210,19 @@ fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel
 
                             Column(
                                 verticalArrangement = Arrangement.SpaceEvenly,
-                                horizontalAlignment = Alignment.CenterHorizontally,
+                                horizontalAlignment = Alignment.Start,
                                 modifier = Modifier.padding(start = 10.dp)
                             ) {
                                 Text(text = drawing.title, style = MaterialTheme.typography.titleMedium)
-                                Text(text = "by ${drawing.author}", style = MaterialTheme.typography.bodySmall)
+                                Text(text = "by ${drawing.userID}", style = MaterialTheme.typography.bodySmall)
+                                // TODO: Figure out timestamp implementation
+//                                drawing.timestamp.let {
+//                                    Text(text = "Uploaded: $it.", style = MaterialTheme.typography.bodySmall)
+//                                }
                                 Button(onClick = {
-                                    //TODO: Save image locally as a copy
-
+                                    drawingViewModel.saveImageAsCopy(drawing.title, 1000, 1000)
                                     navController.navigate("main/$")
-                                })
-                                {
+                                }) {
                                     Text("Copy")
                                 }
                             }
@@ -209,6 +230,7 @@ fun CommunityScreen(viewModel: HomeViewModel, drawingViewModel: DrawingViewModel
                     }
                 }
             }
+
         }
     }
 }
